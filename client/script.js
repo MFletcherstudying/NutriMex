96248934d91866a7ff534746eaaa544cb6d72d1e
Marc Fletcher <marcfletcher@Marcs-MacBook-Pro-2.local>
2023-01-30 11:07:55 -0600

    Update script.js

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
 
   //fecth data from server -> bots input
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://nutrimex.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
